94ed44dbf21efe1595741d1fbc9a53ce23b902da
Aiden Kenny <akenny430@gmail.com>
2020-11-23 02:40:49 -0500

    finished q09

---
+++ hwk/hwk02/R/hwk02-q09.R
@@ -0,0 +1,8 @@
+n <- 2
+gamma <- 0.9
+exp_width <- qt(p = (1 + gamma) / 2, df = n - 1)^2 / n
+while (exp_width >= 1/8) {
+  n <- n + 1
+  exp_width <- qt(p = (1 + gamma) / 2, df = n - 1)^2 / n
+}
+print(n)
