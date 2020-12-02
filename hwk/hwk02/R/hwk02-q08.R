418a08f02dea9ef75a438cd2f72495ba502bc6e4
Aiden Kenny <akenny430@gmail.com>
2020-12-02 03:10:13 -0500

    added plot of power function

--- hwk/hwk02/R/hwk02-q08.R
+++ hwk/hwk02/R/hwk02-q08.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+library(data.table)
+myred <- "#ff6666"
+
 find_c <- function(alpha, n) {
   c <- qchisq(p = alpha, df = n - 1, lower.tail = FALSE)
   return(c)
@@ -13,6 +17,15 @@ power_fun <- function(sig2, sig20, alpha, n) {
   return(val)
 }
 
-power_fun(1, 1, 0.025, 13)
+power_fun(c(1, 2, 3, 4, 5), c(1, 2, 3, 4, 5), 0.025, 13)
+
+ggplot(
+  data.table(sig2 = seq(1, 5, 0.01)),
+  aes(sig2)
+) +
+  geom_function(fun = power_fun, args = list(sig20 = 1, alpha = 0.025, n = 13), color = myred, lwd = 2) +
+  labs(x = expression(sigma^2), y = expression(pi(sigma^2 , delta))) +
+  theme_bw(base_size = 30)
+ggsave("hwk/hwk02/img/q08-power-function.png")
 
 rm(list = ls())
