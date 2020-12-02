18a3a2440cec155cb1865b8006dcedd09d37708d
Aiden Kenny <akenny430@gmail.com>
2020-12-02 03:01:47 -0500

    wrote power function

--- hwk/hwk02/R/hwk02-q08.R
+++ hwk/hwk02/R/hwk02-q08.R
@@ -6,6 +6,13 @@ find_c <- function(alpha, n) {
 c <- find_c(0.025, 13)
 round(c, 3)
 
+power_fun <- function(sig2, sig20, alpha, n) {
+  c <- find_c(alpha, n)
+  calt <- c * sig20 / n
+  val <- pgamma(calt, shape = (n - 1) / 2, rate = n / (2 * sig2), lower.tail = FALSE)
+  return(val)
+}
 
+power_fun(1, 1, 0.025, 13)
 
 rm(list = ls())
