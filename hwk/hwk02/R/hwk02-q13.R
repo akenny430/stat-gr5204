0daa98592604b073e419fa4a7f29c9a717be26e8
Aiden Kenny <akenny430@gmail.com>
2020-12-02 14:05:24 -0500

    wrote function for power and getting parameters

--- hwk/hwk02/R/hwk02-q13.R
+++ hwk/hwk02/R/hwk02-q13.R
@@ -1,8 +1,17 @@
-n <- 16
+power_fun <- function(theta, c, n) {
+  c_alt <- sqrt(n) * (c - theta)
+  val <- pnorm(c_alt, 0, 1, lower.tail = FALSE)
+  return(val)
+}
 
-power_at <- 0.95
+test_info <- function(theta_hyp, theta0, power_at_theta0, n) {
+  c_alt <- qnorm(power_at_theta0, 0, 1, lower.tail = FALSE)
+  c <- theta0 + c_alt / sqrt(n)
+  alpha <- power_fun(theta_hyp, c, n)
+  return(
+    list(c = c, alpha = alpha)
+  )
+}
 
-c_alt <- qnorm(power_at, 0, 1, lower.tail = FALSE)
-
-c <- 1 + c_alt / sqrt(n)
-
+pars <- test_info(0, 1, 0.95, 16)
+purrr::map_dbl(.x = pars, .f = ~round(., 3))
