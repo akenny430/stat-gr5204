dd4e65e4159360b7e62616645be7455314d4ad96
Aiden Kenny <akenny430@gmail.com>
2020-12-04 07:51:37 -0500

    fixed formula

--- hwk/hwk03/R/hwk03-q10.R
+++ hwk/hwk03/R/hwk03-q10.R
@@ -39,7 +39,7 @@ coef_var <- function(df) {
   x <- df[[1]]
   barx <- mean(x)
   n <- nrow(df)
-  xnorm <- sum((x - barx)^2)
+  xnorm <- sum((x - barx)^2)re
   b0 <- 1 / xnorm
   b1 <- 1 / n + barx^2 / xnorm
   cov <- - barx / xnorm
