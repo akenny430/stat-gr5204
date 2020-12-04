124a620d1aa443cb057572f5807445f6e07aba3b
Aiden Kenny <akenny430@gmail.com>
2020-12-04 08:05:29 -0500

    got mse of the combination

---
+++ hwk/hwk03/R/hwk03-q11.R
@@ -0,0 +1,7 @@
+source("hwk/hwk03/R/hwk03-q10.R")
+
+linear_combo_var <- function(df, a, b) {
+  var_info <- coef_var(df)
+  lin_com_coef <- a^2 * var_info[1] + b^2 var_info[2] + 2 * a * b * var_info[4]
+  return(lin_com_coef)
+}
