8ea393cd8239dae027068523d9dd1054fabda2a3
Aiden Kenny <akenny430@gmail.com>
2020-12-04 09:18:16 -0500

    put () to make calculations more explicit

--- hwk/hwk03/R/hwk03-q11.R
+++ hwk/hwk03/R/hwk03-q11.R
@@ -2,10 +2,10 @@ source("hwk/hwk03/R/hwk03-q10.R")
 
 linear_combo_var <- function(df, a, b) {
   var_info <- coef_var(df)
-  lin_com_coef <- a^2 * var_info[1] + b^2 * var_info[2] + 2 * a * b * var_info[4]
-  coef_sig <- purrr::map_dbl(lin_com_coef, ~signif(., 3))
+  lin_com_coef <- ((a)^2 * var_info[1]) + ((b)^2 * var_info[2]) + (2 * a * b * var_info[3])
+  coef_sig <- purrr::map_dbl(lin_com_coef, ~round(., 3))
   names(coef_sig) <- "var(lin_com)"
-  return(lin_com_coef)
+  return(coef_sig)
 }
 
 linear_combo_var(data, 3, -2)
